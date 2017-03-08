bee0137865815f24250153b47d4bc691c09802e9
ajith <ajith@growzippy.com>
2017-03-08 15:40:25 +0530

    coming soon page created

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,18 +1,18 @@
-var config = require('./config');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var morgan = require('morgan');
+var config       = require('./config');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index        = require('./routes/index');
+var users        = require('./routes/users');
 
-var app = express();
-var http = require('http').Server(app);
+var app          = express();
+var http         = require('http').Server(app);
 
-var log4js = require('log4js');
+var log4js       = require('log4js');
 log4js.configure({
     appenders: [
     { type: 'console' },
@@ -21,7 +21,8 @@ log4js.configure({
 
 var logger = log4js.getLogger('GDD');
 logger.setLevel(config.LOG_Level);
-// view engine setup
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -38,25 +39,23 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
 http.listen(config.SERVER_PORT, function(){
-  logger.info('mybadmintonclub server on *:'+config.SERVER_PORT);
+    logger.info('mybadmintonclub server on *:'+config.SERVER_PORT);
 });
-
-//module.exports = app;
--- backend/server/config.js
+++ backend/server/config.js
@@ -1,6 +1,6 @@
 var config         = {};
 
 config.LOG_LEVEL   = 'DEBUG';
-config.SERVER_PORT = 5000;
+config.SERVER_PORT = 5555;
 
 module.exports = config;
